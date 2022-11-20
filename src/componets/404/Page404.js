880668bb075f9dd5976d1be324d16bbba18ae565
Raul Pineda <75541681+fernando5498@users.noreply.github.com>
2022-11-19 19:15:03 -0500

    Add page 404

--- src/componets/404/Page404.js
+++ src/componets/404/Page404.js
@@ -1,8 +1,13 @@
 import React from 'react'
-
+import './Page404.scss'
+import { Link } from "react-router-dom";
 const Page404 = () => {
   return (
-    <div>Page404</div>
+    <div className='Page404 u-paddingSection'>
+      <div className="Page404-h">¿You got lost?</div>
+      <p className="Page404-p">It's a pleasure to have you on my website, so press the button to continue viewing the site</p>
+      <Link to={'/'} className='Button Button--dark'>Get Home</Link>
+    </div>
   )
 }
 
